1fb3d5f1aa63aa6f48499965bf0e0622d62579fb
Quico Saval <quicosv@gmail.com>
2023-07-04 19:23:09 +0200

    Añade las variables de los títulos de las páginas previstas

--- src/variables/titulos.ts
+++ src/variables/titulos.ts
@@ -1,2 +1,11 @@
 const aplicacion = "Agencia de viajes";
 export const home = "Inicio - "+ aplicacion;
+export const clientes = "Gestión de clientes" + aplicacion;
+export const mayoristas = "Gestión de mayoristas - " + aplicacion;
+export const viajes = "Gestión de viajes - " + aplicacion;
+export const ventas = "Gestión de ventas - " + aplicacion;
+export const usuarios = "Gestión de empleados - " + aplicacion;
+export const login = "Inicio de sesión - " + aplicacion;
+export const catalogo = "Catálogo de viajes - " + aplicacion;
+	
+}
