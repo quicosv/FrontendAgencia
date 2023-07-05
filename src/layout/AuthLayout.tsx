aa51f7f603de2721dcfaae1593f062b96034b7d5
Quico Saval <quicosv@gmail.com>
2023-07-05 17:50:00 +0200

    Agrega el layout para la página de login y corrige los errores en el AppRouter

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,2 +1,15 @@
+import { Outlet } from "react-router-dom"
+import { aplicacion } from "../variables/titulos"
+
 export const AuthLayout = () => {
+	return (
+		<>
+		<header>
+			<h1>{aplicacion}</h1>
+		</header>
+		<main className="container mt-2">
+			<Outlet />
+		</main>
+		</>
+	)
 }
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { AuthLayout } from "../layout/AuthLayout";
+import { LoginPage } from "../pages/auth/LoginPage";
 
 export const AppRouter = () => {
 	return (
