5f6db8523be944038d3818784f6501db5a780dfc
Quico Saval <quicosv@gmail.com>
2023-07-05 16:34:08 +0200

    Corrige un error en el archivo de los títulos

--- src/variables/titulos.ts
+++ src/variables/titulos.ts
@@ -1,5 +1,5 @@
 const aplicacion = "Agencia de viajes los 3 mosqueteros";
-export const home = "Inicio - "+ aplicacion;
+export const home = "Inicio - " + aplicacion;
 export const clientes = "Gestión de clientes" + aplicacion;
 export const mayoristas = "Gestión de mayoristas - " + aplicacion;
 export const viajes = "Gestión de viajes - " + aplicacion;
@@ -7,5 +7,3 @@ export const ventas = "Gestión de ventas - " + aplicacion;
 export const usuarios = "Gestión de empleados - " + aplicacion;
 export const login = "Inicio de sesión - " + aplicacion;
 export const catalogo = "Catálogo de viajes - " + aplicacion;
-	
-}
