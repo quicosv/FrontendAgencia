1352b55cd0ab936a6d97940d25ec94cd1c0495e1
Quico Saval <quicosv@gmail.com>
2023-07-06 18:48:18 +0200

    Modifica la plantilla de la agencia para incluir el Navbar y el año dle copyght

--- src/layout/AgenciaLayout.tsx
+++ src/layout/AgenciaLayout.tsx
@@ -1,18 +1,20 @@
-import { Outlet } from "react-router-dom"
-import { aplicacion } from "../variables/titulos"
+import { Outlet } from "react-router-dom";
+import { aplicacion } from "../variables/titulos";
+import { Navbar } from "../components/Navbar";
 
 export const AgenciaLayout = () => {
 	return (
 		<>
-		<header>
-			<h1>{aplicacion}</h1>
-		</header>
-		<main className="container mt-2">
-<Outlet />
-		</main>
-		<footer>
-			<p>&copy; {aplicacion}.</p>
-		</footer>
+			<header>
+				<h1>{aplicacion}</h1>
+				<Navbar />
+			</header>
+			<main className="container mt-2">
+				<Outlet />
+			</main>
+			<footer>
+				<p>&copy; 2023 {aplicacion}.</p>
+			</footer>
 		</>
-	)
-}
+	);
+};
