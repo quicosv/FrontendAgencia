908a0b48f858d6943e130f02e80cfbce0939f31c
Quico Saval <quicosv@gmail.com>
2023-07-06 19:55:09 +0200

    Prueba a mover el foco en el archivo de clientes

--- src/pages/agencia/ClientesPage.tsx
+++ src/pages/agencia/ClientesPage.tsx
@@ -5,8 +5,12 @@ export const ClientesPage = () => {
 	useEffect(() => {
 		document.title = tituloClientes;
 	}, []);
-	const h1Ref = useRef(null);
-
+	const h1Ref = useRef<HTMLHeadingElement>(null);
+useEffect(() => {
+	if (h1Ref.current) {
+		h1Ref.current.focus();
+	}
+}, []);
 	return (
 		<h1 ref={h1Ref} tabIndex={-1}>
 			{h1Clientes}
