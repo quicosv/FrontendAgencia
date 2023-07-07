ee47413631bfd1919c67447114d65e134e65e761
Ken Masters <mskenmasters@hotmail.com>
2023-07-07 20:22:12 +0200

    añade los enlaces de catalogo y login en HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 import { h1Home, tituloHome } from "../variables/titulos";
+import { Link } from "react-router-dom";
 
 export const HomePage = () => {
 	useEffect(() => {
 		document.title = tituloHome;
-	},[]);
+	}, []);
 	return (
-		<h1>{h1Home}</h1>
-	)
-}
+		<>
+			<h1>{h1Home}</h1>
+			<ul>
+				<li>
+					<Link to="/login">Acceso de empleados</Link>
+				</li>
+				<li>
+					<Link to="/catalogo">Ir al catálogo</Link>
+				</li>
+			</ul>
+		</>
+	);
+};
