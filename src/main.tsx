cca0e3b93751267899cb98a5b7e48a43ec52f78b
Ken Masters <mskenmasters@hotmail.com>
2023-07-11 19:59:01 +0200

    actualiza main

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter } from "react-router-dom";
-import { MainApp } from "./pages/MainApp";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import { BrowserRouter } from 'react-router-dom';
+import { MainApp } from './pages/MainApp';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<MainApp />
-		</BrowserRouter>
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <MainApp />
+    </BrowserRouter>
+  </React.StrictMode>
 );
