ebc3ce797823e2f377c869db0f1eb1d25cbb3b56
Quico Saval <quicosv@gmail.com>
2023-07-11 19:01:09 +0200

    Añade el título y la gestión del foco a UsuariosPage

--- src/pages/usuarios/UsuariosPage.tsx
+++ src/pages/usuarios/UsuariosPage.tsx
@@ -1,13 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { UsuariosTable } from './UsuariosTable';
 import { UsuariosForm } from './UsuariosForm';
+import { h1Usuarios, tituloUsuarios } from '../../variables/titulos';
 
 export const UsuariosPage = () => {
   const [refreshUsuarios, setRefreshUsuarios] = useState<boolean>(true);
-
+useEffect(() => {
+  document.title = tituloUsuarios;
+});
+const h1Ref = useRef<HTMLHeadingElement>(null);
+useEffect(() => {
+  if (h1Ref.current) {
+    h1Ref.current.focus();
+  }
+},[]);
   return (
     <>
-      <h1>Usuarios</h1>
+      <h1 ref={h1Ref} tabIndex={-1}>{h1Usuarios}</h1>
       <hr />
       <div className="row">
         <div className="col">
