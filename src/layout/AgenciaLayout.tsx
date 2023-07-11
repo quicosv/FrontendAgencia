785b44d22424f59808f57f0675b3568e140cc76a
Quico Saval <quicosv@gmail.com>
2023-07-11 12:44:19 +0200

    Progege las rutas en agenciaLayout

--- src/layout/AgenciaLayout.tsx
+++ src/layout/AgenciaLayout.tsx
@@ -1,8 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { aplicacion } from "../variables/titulos";
 import { Navbar } from "../components/Navbar";
+import { useContext, useEffect } from "react";
+import { IUsuarioInfoContext } from "../interfaces/context.interface";
+import { AppContext } from "../context/AppContext";
 
 export const AgenciaLayout = () => {
+	const { usuarioInfo } = useContext<IUsuarioInfoContext>(AppContext);
+	const navigate = useNavigate();
+	const location = useLocation();
+	const esRutaAbierta = (ruta: string): boolean => {
+		let resultado;
+		if (ruta === '/' || ruta === '/catalogo') {
+			resultado = true;
+		}
+		else {
+			resultado = false;
+		}
+		return resultado;
+	};
+	useEffect(() => {
+		if (usuarioInfo.rol === 0 && !esRutaAbierta(location.pathname)) {
+			navigate('/login', {
+				replace: true
+			});
+		};
+		if (usuarioInfo.rol === 2 && location.pathname.includes('usuarios')) {
+			navigate('/login', {
+				replace: true
+			});
+		};
+	},[location.pathname]);
 	return (
 		<>
 			<header>
